4df5bdfc8f6f6a21b63133f0f0e6613c39420d43
soyezequielitto-svg <soyezequielitto@gmail.com>
2025-11-20 08:16:30 -0300

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NewGroupResponse } from "../types";
 
-const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_API_KEY });
+const ai = new GoogleGenerativeAI({ apiKey: import.meta.env.VITE_API_KEY });
 const modelId = "gemini-2.5-flash";
 
 export const fetchGroupDetails = async (groupName: string): Promise<NewGroupResponse | null> => {
